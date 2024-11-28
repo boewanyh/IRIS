fa732efe7f79413d5904c66b9bbaabb5bf6f1952
10695144 <wanyuhao@boe.com.cn>
2024-11-28 09:17:23 +0800

    20241128

--- code/src/BOE/CallMDHSB.java
+++ code/src/BOE/CallMDHSB.java
@@ -12,8 +12,15 @@ public class CallMDHSB {
             String action = "MES0023";
             String message = "<Request><TradeCode>1012</TradeCode><ExtUserID>wx001</ExtUserID><DepartmentCode></DepartmentCode><StartDate>2024-11-19</StartDate><EndDate>2024-11-19</EndDate><HospitalId>BJMDYY</HospitalId><test>中文</test></Request>";
 
+            // 将 message 转换为字节数组，模拟 RAW 格式
+            byte[] actionBytes = action.getBytes("ISO-8859-1"); 
+            String decodedaction = new String(actionBytes, "ISO-8859-1");
+            byte[] messageBytes = message.getBytes("ISO-8859-1"); 
+            String decodedMessage = new String(messageBytes, "ISO-8859-1");
+
+
             // 原始字符串
-            String data = "_api_access_key=ak&_api_name=service&_api_nonce=2FDC553E-467F-444F-9C35-802FF7936C68&_api_timestamp=1559467961208&_api_version=1.0&action=" + action + "&message=" + message;
+            String data = "_api_access_key=ak&_api_name=service&_api_nonce=2FDC553E-467F-444F-9C35-802FF7936C68&_api_timestamp=1559467961208&_api_version=1.0&action=" + decodedaction + "&message=" + decodedMessage;
 
             // 密钥
             String secret = "secret";
